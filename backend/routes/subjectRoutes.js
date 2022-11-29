69cd619a2b9196b5a32fe5486b8426d293f68263
Dewang Sharma <dewang@Dewangs-MacBook-Air.local>
2022-11-29 20:19:25 +0530

    modify backend

--- backend/routes/subjectRoutes.js
+++ backend/routes/subjectRoutes.js
@@ -3,16 +3,21 @@ const router = express.Router();
 const Subject = require('../models/subjectModal.js')
 
 router.post('/', (req, res) => {
-    const { courseCode, courseName, teacherName, department, syllabus } = req.body;
+    const { courseCode, courseName, teacherName, department, syllabus, teacherId } = req.body;
     const tempSubject = {
         courseCode: courseCode,
         courseName: courseName,
         teacherName: teacherName,
         department: department,
-        syllabus: syllabus
+        syllabus: syllabus,
+        teacherId: teacherId
     }
     Subject.save(tempSubject);
 
+})
+router.get('/', (req, res) => {
+    sub = Subject.find();
+    res.json(sub)
 })
 
 module.exports = router;
