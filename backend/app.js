c9e6de80029e27c9cfb889c42ca3ec41d7b7a7c6
jasleen <rachitsharma3642@gmail.com>
2022-12-07 13:21:25 +0530

    backend changes

--- backend/app.js
+++ backend/app.js
@@ -8,16 +8,17 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/userModel');
 const routes = require('./routes/userRoutes.js');
 const subjectRoutes = require('./routes/subjectRoutes.js');
-
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 
 mongoose.connect(process.env.MONGO_DB).then((result) =>
-  app.listen(process.env.PORT, (req, res) => {
+  app.listen(process.env.PORT,"192.168.43.23" ,(req, res) => {
     console.log(`mongo connected, listenning on port: ${PORT}`)
   }))
   .catch((err) => console.log(err, 'mongo not connected'));
--- backend/routes/subjectRoutes.js
+++ backend/routes/subjectRoutes.js
@@ -2,22 +2,36 @@ const express = require('express');
 const router = express.Router();
 const Subject = require('../models/subjectModal.js')
 
-router.post('/', (req, res) => {
-    const { courseCode, courseName, teacherName, department, syllabus, teacherId } = req.body;
+router.post('/', async (req, res) => {
+    const { courseCode, courseName, teacherName, department, syllabus, teacherID } = req.body;
+    console.log(req.body)
     const tempSubject = {
         courseCode: courseCode,
         courseName: courseName,
         teacherName: teacherName,
         department: department,
         syllabus: syllabus,
-        teacherId: teacherId
+        teacherID: teacherID
     }
-    Subject.save(tempSubject);
+    console.log(tempSubject)
+    const subject = new Subject(tempSubject)
 
+    const data=await subject.save( (err,response)=>{
+        if(err) console.log("Error Occured");
+        else console.log("Data Added");
+    });
+
+    res.send("Got Your Request")
+    // Subject.save(tempSubject,(err,response)=>{
+    //     if(err) res.send("Error Occured");
+    //     res.send("Subject Saved")
+    // });
 })
 router.get('/', (req, res) => {
-    sub = Subject.find();
-    res.json(sub)
+    sub = Subject.find({},(err,data)=>{
+        if(err) console.log("Error is present",err)
+        res.json(data)
+    });
 })
 
 module.exports = router;
